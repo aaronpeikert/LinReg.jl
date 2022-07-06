bef8db7c03904bc68f3317f87fd357d6f2578284
Aaron Peikert <aaron.peikert@posteo.de>
2022-07-06 16:25:02 +0200

    add documentation

--- src/LinReg.jl
+++ src/LinReg.jl
@@ -2,5 +2,13 @@ module LinReg
     import Statistics.mean
     export squared_loss
     include("loss.jl")
+    """
+    Predict new observations
+
+    ## Arguments
+
+    - `x`: the new oberservations
+    - `β`: the beta weights
+    """
     predict(x, β) = x*β
 end
